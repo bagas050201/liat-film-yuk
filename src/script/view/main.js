99740c32b98b3798d7c0807243e2362034839b51
Muhammad Bagas Pradana <54340164+bagas050201@users.noreply.github.com>
2020-07-29 10:50:58 +0700

    Update main.js

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -6,8 +6,6 @@ const main = () => {
     const searchElement = document.querySelector("search-bar");
     const filmListElement = document.querySelector("film-list");
 
-   
-
    /**sesudah pakai promise */
    const onButtonSearchClicked = () => {
         DataSource.searchFilm(searchElement.value)
@@ -15,13 +13,11 @@ const main = () => {
             .catch(fallbackResult)
     };
 
-   
    //sesudah custom element
    const renderResult = results => {
     filmListElement.film = results;
     };
 
-   
    //sesudah custom element
    const fallbackResult = message => {
     filmListElement.renderError(message);
