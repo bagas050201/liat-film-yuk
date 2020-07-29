04e282f843b06964c4a404f09755e0c948f44929
Muhammad Bagas Pradana <54340164+bagas050201@users.noreply.github.com>
2020-07-29 10:50:06 +0700

    Update data-source.js

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,5 +1,4 @@
-
-/**fetch API**/
+/**fetch API MovieDB**/
 class DataSource {
     static searchFilm(keyword) {
         return fetch('https://api.themoviedb.org/3/search/movie?api_key=93690ebe288617edd2f813074329a79c&query=${keyword}')
